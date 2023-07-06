0ad289e897193fc04a2cc3ffaa335eb529e5e54d
Filipe Moreira Coelho <60034638+Filipe-m@users.noreply.github.com>
2023-07-06 20:07:07 -0300

    favicon and controllers dir

---
+++ articles/ArticlesController.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/articles", (req, res) => {
+  res.send("Rota de artigos");
+});
+module.exports = router;
--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const connection = require("./database/database");
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const connection = require('./database/database')
+const categoriesController = require('./categories/CategoriesController')
+const articlesController = require('./articles/ArticlesController')
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
 
-app.use(express.static("public"));
+app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.use('/', categoriesController)
+app.use('/', articlesController)
 
 connection
   .authenticate()
   .then(() => {
-    console.log("Database connection done!");
+    console.log('Database connection done!')
   })
-  .catch((e) => console.log(e));
+  .catch(e => console.log(e))
 
 app.listen(8080, () => {
-  console.log("Server started on port 8080");
-});
+  console.log('Server started on port 8080')
+})
